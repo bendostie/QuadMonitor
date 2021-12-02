01e881eac7f8b169c95969d0918c09bf5a18ffef
bendostie <bendostie@outlook.com>
2021-12-02 17:22:54 -0500

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,5 +1,7 @@
 #script to set up Quad Monitor database for the first time
 #Benjamin Dostie, 2021
+from datetime import date
+from datetime import datetime
 
 import mysql.connector
 
@@ -11,11 +13,30 @@ conn = mysql.connector.connect(
 cur = conn.cursor()
 cur.execute("DROP DATABASE IF EXISTS QuadMonitor")
 cur.execute("CREATE DATABASE QuadMonitor")
-cur.execute("DROP TABLE IF EXISTS SensorData")
-cur.execute('''CREATE TABLE PC
-                (model CHAR(5) PRIMARY KEY,speed INT, ram INT,hd INT,
-                price DECIMAL(7,2) NOT NULL, 
-                FOREIGN KEY (model) 
-                REFERENCES Product(model)
-                ON DELETE CASCADE
-                ON UPDATE CASCADE)''')
+cur.execute("DROP TABLE IF EXISTS readings")
+cur.execute('''CREATE TABLE devices(
+                DeviceID CHAR(3),
+                NodeID CHAR,
+                DeviceType CHAR                
+                PRIMARY KEY (DeviceID))''')
+cur.execute('''CREATE TABLE readings(
+                DeviceID CHAR(3),
+                DataDate DATE,
+                DataTime TIME,
+                Reading DECIMAL(9, 6),
+                
+                FOREIGN KEY (DeviceID) 
+                REFERENCES devices(DeviceID)
+                ON DELETE SET NULL
+                ON UPDATE CASCADE
+                PRIMARY KEY (DeviceID, DataDate, DataTime))''')
+cur.execute('''INSERT INTO devices(DeviceID, NodeID, DeviceType) 
+            VALUES ('001', 'Arduino_1', 'soil_moisture')''')
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+current_date = now.strftime("'%Y-%m-%d")
+cur.execute('''INSERT INTO readings(DeviceID, DataDate, DataTime, Reading) 
+            VALUES ('001', %s,%s, 3.14)''', [current_time, current_date])
+conn.commit()
+conn.close()
