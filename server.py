be7282ea11f365aadf7c738ba4bfe971acd85819
Benjamin Dostie <bendostie@outlook.com>
2021-12-05 11:23:54 -0500

    adds keyboard intterupt and fixes insert

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
+
 import socket
 import threading
 from datetime import datetime
 ip_address = "0.0.0.0"
-port = 81
+port = 2001
 
 import mysql.connector
 
@@ -16,29 +17,41 @@ cur.execute("USE QuadMonitor")
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip_address, port))
 server.listen()
-while True:
-    print("Waiting for connection")
-    connection, client = server.accept()
- 
-    try:
-        print("Connected to client IP: {}".format(client))
-         
-        # Receive and print data 32 bytes at a time, as long as the client is sending something
-        while True:
-            data = connection.recv(32)
-            print("Received data: {}".format(data))
-            try:
-                id = data[0:4]
-                reading = float(data[4:])
-                now = datetime.now()
-                current_time = now.strftime("%H:%M:%S")
-                current_date = now.strftime("'%Y-%m-%d")
-                cur.execute('''INSERT INTO readings(DeviceID, DataDate, DataTime, Reading) 
-            VALUES ('001', %s,%s, 3.14)''', [current_time, current_date])
-            except:
-                print("invalid data")
-            if not data:
-                break
- 
-    finally:
-        connection.close()
+try:
+    while True:
+        print("Waiting for connection")
+        connection, client = server.accept()
+     
+        try:
+            print("Connected to client IP: {}".format(client))
+             
+            # Receive and print data 32 bytes at a time, as long as the client is sending something
+            while True:
+                data = connection.recv(32)
+                print("Received data: {}".format(data))
+                
+                try:
+                    id_code= data[0:3]
+                    reading = float(data[3:])
+                    print("data")
+                    print(id_code)
+                    print(reading)
+                    now = datetime.now()
+                    current_time = now.strftime("%H:%M:%S")
+                    current_date = now.strftime("%Y-%m-%d")
+                    cur.execute('''INSERT INTO readings(DeviceID, DataDate, DataTime, Reading) 
+                VALUES (%s, %s,%s, %s)''', [id_code, current_date, current_time, reading])
+                    conn.commit()
+                except Exception as ex:
+                    print("invalid data")
+                    print(ex)
+                    break
+                if not data:
+                    break
+     
+        finally:
+            connection.close()
+except KeyboardInterrupt:
+    server.close()
+    conn.commit()
+    conn.close()
